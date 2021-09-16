9a03195d775a6450cf6c4e7d5ac701bd7d0f977f
GSdaBlessedFist <gsdablessedfist@gmail.com>
2021-09-16 00:31:30 -0500

    1)github button situated > linked to github project...NEXT:: close button needed + animation

--- public/js/gridwork.js
+++ public/js/gridwork.js
@@ -3,6 +3,7 @@ const p = console.log;
 const technicallyButton= document.getElementById("technically-button");
 const primeContainerGrid= document.getElementById("prime-container");
 const skillsColumn= document.getElementById("skills-column");
+const githubButton= document.getElementById("github-button");
 const gridColumnsConfigs = {
 	"closed": "50% 25.9% .1% 25%",
 	"open": "25% 30% 20% 25%"
@@ -11,7 +12,10 @@ const gridColumnsConfigs = {
 document.addEventListener('click',(e)=>{
 	e.preventDefault();
 	if(e.target==technicallyButton){
-		gsap.to(primeContainerGrid,{duration:.65,gridTemplateColumns:gridColumnsConfigs.open,ease: "elastic.out(1, 0.6)"});
+		gsap.to(primeContainerGrid,{duration:.95,gridTemplateColumns:gridColumnsConfigs.open,ease: "elastic.out(1, 0.7)"});
 		gsap.to(skillsColumn,{duration:.65,opacity:1,delay:.35});
+	}
+	if(e.target==githubButton){
+		window.open("https://github.com/GSdaBlessedFist/Simple-Cosmic-blog","_blank");
 	}
 })
