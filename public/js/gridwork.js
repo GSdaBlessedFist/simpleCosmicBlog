7b45cd36d2b4616a48a63bd8bb01c82a109cf7ca
GSdaBlessedFist <gsdablessedfist@gmail.com>
2021-09-15 20:38:11 -0500

    1)added technically button...2)it's animation implemented, exposing skills column...NEXT::  github/close section button > github + x button...->close animation

--- public/js/gridwork.js
+++ public/js/gridwork.js
@@ -1,1 +1,17 @@
 //gridwork.js
+const p = console.log;
+const technicallyButton= document.getElementById("technically-button");
+const primeContainerGrid= document.getElementById("prime-container");
+const skillsColumn= document.getElementById("skills-column");
+const gridColumnsConfigs = {
+	"closed": "50% 25.9% .1% 25%",
+	"open": "25% 30% 20% 25%"
+}
+
+document.addEventListener('click',(e)=>{
+	e.preventDefault();
+	if(e.target==technicallyButton){
+		gsap.to(primeContainerGrid,{duration:.65,gridTemplateColumns:gridColumnsConfigs.open,ease: "elastic.out(1, 0.6)"});
+		gsap.to(skillsColumn,{duration:.65,opacity:1,delay:.35});
+	}
+})
